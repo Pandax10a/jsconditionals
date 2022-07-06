a79d8dbe98c708102c9b737b3e53b42da6d17321
panda1 <blueblue0011@gmail.com>
2022-07-05 19:49:34 -0700

    added else statement

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-let user_points = 88;
-if (user_points >75) {
-    console.log("crongratulation");
+let user_points = 70;
+if (user_points >= 75) {
+    console.log("congratulation");
+} else {
+    console.log("so close too bad");
 }
